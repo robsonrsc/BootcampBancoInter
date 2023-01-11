f166071a15d07d54fc7208f2b9d86c242bc17e95
robsonrsc <robsonrsc.stos@gmail.com>
2023-01-11 11:54:37 -0300

    Primeiro Exercicio

---
+++ src/main/java/EstruturasDeRepeticao/IdadeAluno.java
@@ -0,0 +1,28 @@
+package EstruturasDeRepeticao;
+
+import java.util.Scanner;
+
+public class IdadeAluno {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+
+        while (true) {
+            System.out.println("\n\nInsira o nome: ");
+            String nome = scan.next();
+            if (nome.equals("0")) break;
+            System.out.println("Insira a idade: ");
+            int idade = scan.nextInt();
+
+            
+
+            System.out.printf("\nNome do aluno e %s", nome);
+            System.out.printf("\nA idade e %d", idade);
+        }
+
+        System.out.println("teste de continuidade");
+
+    }
+}
